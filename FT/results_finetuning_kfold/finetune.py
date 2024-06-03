a1fc74304d5dfeeb550ed4934e2e82a51640bae8
liangtelkamp <73824626+liangtelkamp@users.noreply.github.com>
2024-06-03 16:05:36 +0200

    Add files via upload

---
+++ FT/results_finetuning_kfold/finetune.py
@@ -0,0 +1,316 @@
+import torch
+import pandas as pd
+import numpy as np
+import os
+import sys
+import re
+from collections import Counter
+import random
+import torch
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+import torch
+from transformers import AdamW, get_linear_schedule_with_warmup
+
+import numpy as np
+import pandas as pd
+import torch
+import torch.distributions as td
+import torch.utils.data as data
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+from transformers.trainer_utils import set_seed
+
+import os
+import re
+import json
+
+
+# generating k_fold data split 
+data = torch.load('data_dictionary.pt')
+i_values = [5, 15, 25, 35]
+k_fold_data = {0:{}, 1: {}, 2:{}, 3:{}}
+k = 0
+for i in i_values:
+    train_data_small = {k: v for k, v in data.items() if v['paragraph_num'] < i} # 0:i
+    val_data_small = {k: v for k, v in data.items() if v['paragraph_num'] >= i and v['paragraph_num'] < i+10} # i : i+10
+    train_data_small.update({k: v for k, v in data.items() if v['paragraph_num'] >= i+10 and v['paragraph_num'] < 45}) #i+10 : 45
+    test_data = {k: v for k, v in data.items() if v['paragraph_num'] >= 45}
+
+    k_fold_data[k]['train_data'] = train_data_small
+    k_fold_data[k]['val_data'] = val_data_small
+    k_fold_data[k]['test_data'] = test_data
+    print(f'k_fold statistics \nk: {k}\ntrain: {len(train_data_small)}\nval: {len(val_data_small)}\ntest:{len(test_data)}\n')
+    k += 1
+
+def set_seed(seed):
+	random.seed(seed)
+	np.random.seed(seed)
+	torch.manual_seed(seed)
+	torch.cuda.manual_seed_all(seed)
+
+#Switching to GPU
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+class log_probs:
+
+    def __init__(self, model, tokenizer):
+        self.model = model
+        self.tokenizer = tokenizer
+
+        self.bos_token = '<|endoftext|>'
+        self.pad_token = '<|endoftext|>'
+
+    def get_log_prob_from_loss(self, sentence):
+        # print(f'sentence = {sentence}')
+        """Receives a sentence and produces its probability under the model"""
+        inputs =  self.tokenizer(f"{self.bos_token} {sentence}", return_tensors="pt")
+        inputs.to(device)
+        # with torch.no_grad():
+        # print(inputs["input_ids"])
+        outputs = self.model(**inputs, labels=inputs["input_ids"])
+        loss = outputs.loss
+        log_prob = - loss*( len(inputs["input_ids"][0]) - 1) #multiplying by length since the total loss
+        #was the average of losses, 1/N Sigma(L_CE) and subtracting 1 to not account for the bos_token
+
+        return log_prob
+        # return log_prob.item()
+
+    def get_log_probs_from_prob_dist(self, sentences):
+        """Receives a list of sentences and returns a list of tensors with the probabilities of each sentence under the model"""
+
+        #We pad to match the length of the longest sentence
+        inputs = self.tokenizer(sentences, padding='longest', return_length=True, return_special_tokens_mask=True,
+                                return_tensors="pt")
+        inputs.to(device)
+
+        #The observation per position is shifted with respect to the input
+        obs_ids = torch.roll(inputs['input_ids'], -1, -1)
+
+        # with torch.no_grad():
+        outputs = self.model(inputs["input_ids"])
+        output_logits = outputs.logits
+
+        pT = td.Categorical(logits=output_logits)
+        tok_log_probs = pT.log_prob(obs_ids).to(device)
+        zeroes = torch.zeros(tok_log_probs.shape[0],tok_log_probs.shape[1]).to(device)
+
+        #Log probabilities for non-padded tokens
+        tok_log_probs_no_pad = torch.where(obs_ids == self.tokenizer.eos_token_id, zeroes, tok_log_probs)
+
+        return tok_log_probs_no_pad.sum(-1) #the log_prob of each sentence
+
+    def get_log_probs_from_loss_sien(self, sentences):
+        #We pad to match the length of the longest sentence
+        log_probs = []
+        for sentence in sentences:
+            log_probs.append(self.get_log_prob_from_loss(sentence))
+
+        return torch.tensor(log_probs).to(device)
+
+
+class cond_log_probs:
+    def __init__(self, model, tokenizer):
+        self.model = model
+        self.tokenizer = tokenizer
+        self.log_probs_obj = log_probs(self.model, self.tokenizer)
+
+    def get_log_prob_context(self, context):
+        self.log_prob_context = self.log_probs_obj.get_log_prob_from_loss(context)
+        return self.log_prob_context
+
+    def conditional_log_prob_of_word_given_context(self, word, context):
+        """Receives one word and a context and returns the log probability of the word given the context under the model"""
+        context_and_word = context + ' ' + word
+        log_prob_context_and_word = self.log_probs_obj.get_log_prob_from_loss(context_and_word)
+        log_prob_conditional_on_word = log_prob_context_and_word - self.log_prob_context
+
+        return(log_prob_conditional_on_word)
+
+    def conditional_log_prob_of_word_list_given_context(self, words, context, log_prob_context):
+        """Receives a list of words and a context and returns a list of log probabilities of the words given
+           the context under the model"""
+        bos_token = '<|endoftext|>'
+        sentences = [f'{self.tokenizer.bos_token} {context} {x}' for x in words]
+        log_probs_context_and_words = self.log_probs_obj.get_log_probs_from_prob_dist(sentences)
+
+        # sentences = [f'{context} {x}' for x in words] #each sentence is the context and one of the words # SIEN
+        #log_probs_context_and_words= self.log_probs_obj.get_log_probs_from_loss_sien(sentences) # sien
+
+        log_probs_conditional_on_words = log_probs_context_and_words - log_prob_context
+
+        # for i, prob in enumerate(log_probs_conditional_on_words):
+        #     if prob > 0:
+        #         print(f' log probability conditional > 0 {prob}')
+        #         print(f'prob = {torch.exp(prob)}')
+        #         print(f'log probs = {log_probs_context_and_words[i]} - {log_prob_context}')
+        #         print(f'w = {words[i]}')
+        #         print(f'context = {context}')
+        #         print(f' q(v, c) = {torch.exp(log_probs_context_and_words[i])}')
+        #         print(f' q(c) = {torch.exp(torch.tensor(log_prob_context))}')
+
+        return log_probs_conditional_on_words  # sien
+        # return([x.item() for x in log_probs_conditional_on_words])
+def CE_variability(cond_log_probs_obj, context, words, human_probs):
+
+    log_prob_context = cond_log_probs_obj.get_log_prob_context(context)
+    log_probs_cond = cond_log_probs_obj.conditional_log_prob_of_word_list_given_context(words, context, log_prob_context)
+
+    loss = torch.sum( - human_probs*log_probs_cond)
+
+    cond_probs = torch.exp(log_probs_cond).detach().cpu().numpy()
+    human_probs = human_probs.detach().cpu().numpy()
+
+    return loss, cond_probs, human_probs
+
+
+
+def train_one_fold(model, tokenizer, optimizer, scheduler, epochs, train_data_small, val_data_small, k, dict_parameters):
+    training_stats = []
+    train_losses = []
+    train_loss_avg = []
+    cond_log_probs_obj = cond_log_probs(model, tokenizer)
+    i_list = [20, 100, 150, 200, 300, 400, 500]
+    i_track_dict = {20: [], 100: [], 150: [], 200:[], 300:[], 400:[], 500:[]}
+    print(f'Training fold {k}...')
+    for epoch in range(epochs):
+        print('======== Epoch {:} / {:} ========'.format(epoch + 1, epochs))
+
+        total_train_loss = 0
+        model.train()
+
+        i = 0
+        for context_dict in train_data_small.values():
+
+            context = ' '.join(context_dict['context'])
+            words = [word for word, _ in context_dict['human_next_word_pred']]
+            human_probs = torch.tensor([prob for _, prob in context_dict['human_next_word_pred']]).to(device)
+            model.zero_grad()
+
+            ce_loss, cond_probs, human_probs = CE_variability(cond_log_probs_obj, context, words, human_probs)
+
+            i += 1
+            if i%100 == 0:
+                # print(f'avg loss {i} = {total_train_loss/i}')
+                train_loss_avg.append(total_train_loss/i)
+            ce_loss.backward()
+            optimizer.step()
+            scheduler.step()
+
+            train_losses.append(ce_loss.item())
+            total_train_loss += ce_loss.item()
+
+            if i in i_list:
+                i_track_dict[i].append((cond_probs.tolist(), human_probs.tolist()))
+
+            # if i == 201:
+            #     break
+
+        avg_train_loss = total_train_loss/len(train_data_small)
+
+        print("")
+        print("  Average training loss: {0:.2f}".format(avg_train_loss))
+        # print("  Training epoch took: {:}".format(training_time))
+
+        print("")
+        print("Running Validation...")
+
+
+        total_val_loss = 0
+
+        i=0
+
+        for context_dict in val_data_small.values():
+
+            context = ' '.join(context_dict['context'])
+            words = [word for word, _ in context_dict['human_next_word_pred']]
+            human_probs = torch.tensor([prob for _, prob in context_dict['human_next_word_pred']]).to(device)
+
+            with torch.no_grad():
+
+                ce_loss, cond_probs, human_probs = CE_variability(cond_log_probs_obj, context, words, human_probs)
+
+            i += 1
+            # if i%100 == 0:
+            #     # print(f'avg loss {i} = {total_val_loss/i}')
+            total_val_loss += ce_loss.item()
+
+
+        avg_val_loss = total_val_loss/len(val_data_small)
+
+        print("")
+        print("  Average validation loss: {0:.2f}".format(avg_val_loss))
+
+        training_stats.append({'epoch': epoch + 1,
+                                'Training loss': avg_train_loss,
+                                'Valid loss': avg_val_loss})
+
+    print('')
+    print('training complete!')
+    print(training_stats)
+
+    model_version = f'test_model_fold_{k}'
+    #Save training statistics (training and validation losses)
+    with open('training_stats' + model_version + '.json', 'w') as fp:
+        json.dump(training_stats, fp)
+        json.dump(train_losses, fp)
+        json.dump(i_track_dict, fp)
+        
+
+    #Saving model
+    output_dir = f'./test_model_fold_{k}/'
+
+    # Create output directory if needed
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # Save a trained model, configuration and tokenizer. They can then be reloaded using 'from_pretrained()'
+    gpt2_provo_fine_tuned = model.module if hasattr(model, 'module') else model  # Take care of distributed/parallel training
+    gpt2_provo_fine_tuned.save_pretrained(output_dir)
+    tokenizer.save_pretrained(output_dir)
+
+    with open('training_parameters' + model_version + '.json', 'w') as fp:
+        json.dump(dict_parameters, fp)
+
+
+for k in range(4):
+    # ensuring all dropout is off
+    model = GPT2LMHeadModel.from_pretrained('gpt2', resid_pdrop = 0., embd_pdrop = 0., attn_pdrop = 0., summary_first_dropout = 0.)
+    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+    tokenizer.pad_token = tokenizer.eos_token
+
+    seed = 0
+    set_seed(seed)
+
+    # freeze all parameters
+    for param in model.parameters():
+        param.requires_grad = False
+
+    # Choose which layers we would like to continue training.
+    # We only want to update the weights of the last layer
+
+    module_to_unfreeze = [model.transformer.h[11].ln_1, model.transformer.h[11].ln_2, model.transformer.h[10].ln_1, model.transformer.h[10].ln_2, model.transformer.ln_f]
+
+    for module in module_to_unfreeze:
+        for param in module.parameters():
+            param.requires_grad = True
+
+    # print(f'model = {model}')
+
+    lr = 0.0001
+    # model = model.to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr = lr)
+    epochs = 10 # set number of epochs
+
+    # Total number of training steps is [number of batches] x [number of epochs].
+    total_steps = len(train_data_small) * epochs
+
+    # Create the learning rate scheduler. This changes the learning rate as the training loop progresses
+    scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps = 10, num_training_steps = total_steps)
+
+    model = model.to(device)
+
+    dict_parameters = {'epochs': epochs, 'lr': lr}
+    print('dict paramters')
+    train_data_small = k_fold_data[k]['train_data']
+    val_data_small = k_fold_data[k]['val_data']
+
+    train_one_fold(model, tokenizer, optimizer, scheduler, epochs, train_data_small, val_data_small, k, dict_parameters)
---
+++ FT/resutls_finetune_kfold_lmhead/eval.py
@@ -0,0 +1,327 @@
+import pandas as pd
+import numpy as np
+import os
+import sys
+import re
+from collections import Counter
+import random
+import torch
+import os
+import re
+import json
+
+def preprocess_provo_corpus(provo):
+    """Reading the raw Provo Corpus dataset and create a dictionary with all useful
+    information we might need from it"""
+    predict_norms = pd.read_csv(provo, sep='\t')
+    paragraphs = predict_norms.groupby('Text_ID')['Text'].max()
+
+    provo_processed = {}
+    count = 0
+    for text_id in range(1,56): #iterate over all provo paragraphs
+        for word_num in predict_norms[predict_norms['Text_ID'] == text_id]['Word_Number'].unique(): #iterating over all words in each text
+            word_dist = predict_norms[(predict_norms['Text_ID'] == text_id) & (predict_norms['Word_Number'] == word_num)]
+            unique_human_words = word_dist['Response'].unique() #all human answered words for each context
+            unique_word_dist = []
+            for word in unique_human_words:
+                unique_word_count = sum(word_dist[word_dist['Response'] == word]['Response_Count']) #getting all counts of the unique word and summing them
+                unique_word_dist.append((word, unique_word_count))
+
+            provo_processed[count] = {}
+            provo_processed[count]['context_with_original_word'] = paragraphs[text_id].split(' ')[:int(word_num)]
+            provo_processed[count]['context'] = paragraphs[text_id].split(' ')[:(int(word_num)-1)]
+            provo_processed[count]['original_positioning'] = {'text_id':text_id, 'word_num':word_num}
+            provo_processed[count]['human_next_word_pred'] = unique_word_dist
+
+            count = count + 1
+
+    return provo_processed
+
+input_data = os.path.join(os.getcwd(), 'Provo_Corpus.tsv')
+data = preprocess_provo_corpus(input_data)
+
+def get_estimator(elements):
+    """Get the MLE estimate given all words (probability of a word equals its relative frequency)"""
+    c = Counter(elements)
+    support = list(c.keys())
+    counts = list(c.values())
+    probs = [count / sum(counts) for count in counts]
+
+    return (support, probs)
+
+def get_common_support(support1, support2):
+    """Receives supports from two distributions and return all elements appearing in at least one of them"""
+    return set(support1).union(set(support2))
+
+def change_support(old_support, old_probs, new_support):
+    """Create new support by adding elements to a support that did not exist before
+     (hence, their probability value is 0)"""
+    new_probs = []
+    for item in new_support:
+        if item in old_support:
+            ind = old_support.index(item)
+            new_probs.append(old_probs[ind])
+        else:
+            new_probs.append(0)
+    return list(new_support), new_probs
+
+def get_tvd(probs1, probs2):
+    """Receives the probabilities of 2 distributions to compare and returns their TVD (Total Variation Distance)"""
+    tvd = np.sum(np.abs(np.array(probs1) - np.array(probs2)))/2
+    return tvd
+
+def get_oracle_elements(words, seed = 0):
+    """We receive a list of words and we create two disjoint subsets
+    from it by sampling without replacement from them.
+    We return the two disjoint sets of words"""
+    random.seed(seed)
+
+    #if the length of the list is odd, we remove one element at random to make the list even,
+    #to ensure the two disjoint subsets are of equal length
+    if (len(words) % 2 == 1):
+        remove_word = random.sample(words, 1)
+        words.remove(remove_word[0])
+
+    #We sample the words that will belong in the first subset and create the second subset by removing
+    #from the full word list the ones sampled in the first subset
+    subset1 = random.sample(words, len(words)//2)
+    subset2 = words.copy()
+    for item in subset1:
+        subset2.remove(item)
+
+    return subset1, subset2
+
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+import torch
+#Switching to GPU
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(device)
+
+
+
+def get_model_samples(model, tokenizer, context, n_samples = 5, add_tokens = 5, seed = 0, pad_token = '<|endoftext|>', top_k = 0):
+    """Given a context we return the words that were generated during ancestral sampling for n_samples"""
+    # inputs_ids = tokenizer(context, return_tensors="pt")['input_ids']
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    inputs_ids = tokenizer(context, return_tensors="pt")['input_ids'].to(device)
+    # inputs_ids.to(device)
+    torch.manual_seed(seed)
+    # outputs = model.generate(inputs_ids, do_sample=True, num_beams = 1, num_return_sequences= n_samples,
+    #                          max_new_tokens = add_tokens, pad_token_id = pad_token, top_k= top_k)
+    outputs = model.generate(inputs_ids, do_sample=True, num_beams = 1, num_return_sequences= n_samples,
+                             max_new_tokens = add_tokens) #This samples unbiasedly from the next-token distribution of the model for add_tokens tokens, n_samples times
+
+    outputs = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+
+    #Remove context to keep only generated text
+    outputs = [x.replace(context + ' ', '').replace(context, '').replace('\n', '') for x in outputs]
+    #Remove punctuation
+    outputs = [re.sub(r'[^\w\s]', '', x) for x in outputs] #removing punctuation
+    list_of_words = [x.split(' ') for x in outputs]
+
+    # print(f'len outputs = {len(list_of_words)}')
+
+    sampled_words = []
+    for generation in list_of_words:
+        if set(generation) == {''}:
+            sampled_words.append('Failed to generate word')
+        else:
+            sampled_words.append(next(x.lower() for x in generation if x))
+
+    return sampled_words
+
+# generating k_fold data split
+data_dict = torch.load('data_dictionary.pt')
+i_values = [5, 15, 25, 35]
+k_fold_data = {0:{}, 1: {}, 2:{}, 3:{}}
+k = 0
+for i in i_values:
+    train_data_small = {k: v for k, v in data_dict.items() if v['paragraph_num'] < i} # 0:i
+    val_data_small = {k: v for k, v in data_dict.items() if v['paragraph_num'] >= i and v['paragraph_num'] < i+10} # i : i+10
+    train_data_small.update({k: v for k, v in data_dict.items() if v['paragraph_num'] >= i+10 and v['paragraph_num'] < 45}) #i+10 : 45
+    test_data = {k: v for k, v in data_dict.items() if v['paragraph_num'] >= 45}
+
+    k_fold_data[k]['train_data'] = train_data_small
+    k_fold_data[k]['val_data'] = val_data_small
+    k_fold_data[k]['test_data'] = test_data
+    print(f'k_fold statistics \nk: {k}\ntrain: {len(train_data_small)}\nval: {len(val_data_small)}\ntest:{len(test_data)}\n')
+    k += 1
+
+def get_tvd_value_human_model(i, test_data, model, tokenizer):
+    test_data = test_data[i]
+    context = ' '.join(test_data['context'])
+
+    # print(f'context test = {context}')
+    # print(f'context data = {data[i]["context"]}')
+
+    model_samples = get_model_samples(model, tokenizer, context, n_samples = 40)
+
+    #Remove samples for which we failed to generate a complete word
+    model_samples = [x for x in model_samples if x != 'Failed to generate word']
+
+    l_words = [[item[0]]*int(item[1]) for item in data[i]['human_next_word_pred']]
+    l_words = [item for row in l_words for item in row]
+    human_samples = l_words.copy()
+
+    # print(f'model samples = {model_samples}')
+    # print(f'human samples = {human_samples}')
+
+    support_human, probs_human = get_estimator(human_samples)
+    support_model, probs_model = get_estimator(model_samples)
+
+    common_support = get_common_support(support_human, support_model)
+
+    support_human, probs_human = change_support(support_human, probs_human, common_support)
+    support_model, probs_model = change_support(support_model, probs_model, common_support)
+
+    tvd = get_tvd(probs_human, probs_model)
+
+    return model_samples, tvd
+
+def get_tvd_value_human_human(i):
+
+    l_words = [[item[0]]*int(item[1]) for item in data[i]['human_next_word_pred']]
+    l_words = [item for row in l_words for item in row]
+    oracle1, oracle2 = get_oracle_elements(l_words)
+
+    #Get the MLE estimates given the samples
+    support_or1, probs_or1 = get_estimator(oracle1)
+    support_or2, probs_or2 = get_estimator(oracle2)
+
+    common_support = get_common_support(support_or1, support_or2)
+
+    support_or1, probs_or1 = change_support(support_or1, probs_or1, common_support)
+    support_or2, probs_or2 = change_support(support_or2, probs_or2, common_support)
+
+    tvd = get_tvd(probs_or1, probs_or2)
+
+    return l_words, tvd
+
+# # evaluate baseline on train data 
+# tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+# tokenizer.pad_token = tokenizer.eos_token
+# model = GPT2LMHeadModel.from_pretrained("gpt2")
+
+# test_data =  k_fold_data[0]['train_data'] # SIEN
+# tvd_human_model_values = []
+# tvd_human_human_values = []
+
+# train_data_stats = {}
+
+# k = 0
+# for i in test_data.keys():
+#     model_samples, tvd_human_model = get_tvd_value_human_model(i, test_data, model, tokenizer)
+#     human_samples, tvd_human_human = get_tvd_value_human_human(i) # SIEN
+
+#     tvd_human_model_values.append(tvd_human_model)
+#     tvd_human_human_values.append(tvd_human_human)
+
+#     if k%50==0:
+#         print(f'--------------------')
+#         print(f'k = {k}')
+#         print(f'tvd human model = {tvd_human_model}')
+#         print(f'tvd human human = {tvd_human_human}')
+
+    
+
+#     k+= 1
+
+# train_data_stats['tvd_human_human'] = tvd_human_human_values
+# train_data_stats['tvd_human_model'] = tvd_human_model_values
+
+
+# with open('tvd_values_baseline_trainset' + '.json', 'w') as fp:
+#     json.dump(train_data_stats, fp)
+
+
+# LOAD K-FOLD MODELS
+kfold_finetuned_models = ['/home/scur1303/nlp2_sien/results_finetune_kfold_lmhead/lmhead_model_fold_0', '/home/scur1303/nlp2_sien/results_finetune_kfold_lmhead/lmhead_model_fold_1', '/home/scur1303/nlp2_sien/results_finetune_kfold_lmhead/lmhead_model_fold_2', '/home/scur1303/nlp2_sien/results_finetune_kfold_lmhead/lmhead_model_fold_3']
+kfold_tvd_stats = {0: {}, 1: {}, 2:{}, 3:{}}
+sample_stats = {}
+for f in range(4):
+    print(f'\nFOLD = {f}\n')
+    tokenizer = GPT2Tokenizer.from_pretrained(kfold_finetuned_models[f])
+    tokenizer.pad_token = tokenizer.eos_token
+    model = GPT2LMHeadModel.from_pretrained(kfold_finetuned_models[f]).to(device)
+
+    test_data =  k_fold_data[f]['test_data'] # SIEN
+    tvd_human_model_values = []
+    tvd_human_human_values = []
+    model_samples_list = []
+    human_samples_list = []
+
+    k = 0
+    for i in test_data.keys():
+        model_samples, tvd_human_model = get_tvd_value_human_model(i, test_data, model, tokenizer)
+        human_samples, tvd_human_human = get_tvd_value_human_human(i) # SIEN
+
+        tvd_human_model_values.append(tvd_human_model)
+        tvd_human_human_values.append(tvd_human_human)
+        model_samples_list.append(model_samples)
+        human_samples_list.append(human_samples)
+
+        if k%50==0:
+            print(f'--------------------')
+            print(f'k = {k}')
+            print(f'tvd human model = {tvd_human_model}')
+            print(f'tvd human human = {tvd_human_human}')
+        
+        k+= 1
+    
+    kfold_tvd_stats[f]['tvd_human_human'] = tvd_human_human_values
+    kfold_tvd_stats[f]['tvd_human_model'] = tvd_human_model_values
+
+    kfold_tvd_stats[f]['human_samples'] = tvd_human_model_values
+    kfold_tvd_stats[f]['model_samples'] = tvd_human_model_values
+    
+
+    
+with open('ft_lmhead_tvd_samples' + '.json', 'w') as fp:
+    json.dump(kfold_tvd_stats, fp)
+
+# BASELINE samples
+# kfold_finetuned_models = ['/home/scur1303/nlp2_sien/resutls_finetuning_kfold/test_model_fold_0', '/home/scur1303/nlp2_sien/resutls_finetuning_kfold/test_model_fold_1', '/home/scur1303/nlp2_sien/resutls_finetuning_kfold/test_model_fold_2', '/home/scur1303/nlp2_sien/resutls_finetuning_kfold/test_model_fold_3']
+# kfold_tvd_stats = {0: {}, 1: {}, 2: {}, 3: {}}
+# sample_stats = {}
+# for f in range(1):
+#     print(f'\nFOLD = {f}\n')
+# tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+# tokenizer.pad_token = tokenizer.eos_token
+# model = GPT2LMHeadModel.from_pretrained("gpt2").to(device)
+
+# test_data =  k_fold_data[0]['test_data'] # SIEN
+# tvd_human_model_values = []
+# tvd_human_human_values = []
+# model_samples_list = []
+# human_samples_list = []
+
+# k = 0
+# for i in test_data.keys():
+#     model_samples, tvd_human_model = get_tvd_value_human_model(i, test_data, model, tokenizer)
+#     human_samples, tvd_human_human = get_tvd_value_human_human(i) # SIEN
+
+#     tvd_human_model_values.append(tvd_human_model)
+#     tvd_human_human_values.append(tvd_human_human)
+#     model_samples_list.append(model_samples)
+#     human_samples_list.append(human_samples)
+
+#     if k%50==0:
+#         print(f'--------------------')
+#         print(f'k = {k}')
+#         print(f'tvd human model = {tvd_human_model}')
+#         print(f'tvd human human = {tvd_human_human}')
+    
+#     k+= 1
+
+# kfold_tvd_stats[f]['tvd_human_human'] = tvd_human_human_values
+# kfold_tvd_stats[f]['tvd_human_model'] = tvd_human_model_values
+
+# sample_stats['human_samples'] = human_samples_list
+# sample_stats['model_samples'] = model_samples_list
+# sample_stats['tvd_human_model'] = tvd_human_model_values
+# sample_stats['tvd_human_human'] = tvd_human_human_values
+
+# with open('baseline_samples_tvd' + '.json', 'w') as fp:
+#     json.dump(sample_stats, fp)
